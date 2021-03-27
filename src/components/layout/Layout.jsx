184b5c1b98e12cfe3893a79858dea000f897587c
siljaros1994 <70152073+siljaros1994@users.noreply.github.com>
2021-03-27 23:43:35 +0000

    Update Layout.jsx

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,6 +1,16 @@
-
+import React from 'react';
+import s from './Layout.scss';
 // TODO sækja Sass
 
-export function Layout({ /* todo senda inn efni */ }) {
+export function Layout({children}) {
   // TODO setja upp layout fyrir vef
+  return (
+    <div className={layout}>
+      <h1>RÚV fréttir</h1>
+      <main className={s.layout_main}>{children}</main>
+      <footer>
+        <p>Fréttir frá</p><a href="http://www.ruv.is">RÚV</a>
+      </footer>
+    </div>
+  );
 }
