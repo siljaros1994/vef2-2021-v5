8c087d64b334802bd6b4dfe5092d1691cacd3501
siljaros1994 <70152073+siljaros1994@users.noreply.github.com>
2021-03-27 23:43:41 +0000

    Update NewsList.jsx

--- src/components/news-list/NewsList.jsx
+++ src/components/news-list/NewsList.jsx
@@ -1,7 +1,46 @@
 import React, { useEffect, useState } from 'react';
+import { News } from "../news/News";
+import fetch from 'node-fetch';
+import s from "./NewsList.scss";
 
 const apiUrl = process.env.REACT_APP_API_URL;
 
 export function NewsList() {
   // TODO sækja yfirlit fréttaflokka
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const [data, setData] = useState(false);
+
+  useEffect(() => {
+    async function fetchData() {
+      setLoading(true);
+      setError(null);
+      let json;
+        try {
+          const result = await fetch(apiUrl);
+        
+          if(!result.ok) {
+            throw new Error('Ekki tókst að sækja gögn');
+          }
+    
+          json = await result.json();
+        } catch (e) {
+          setError('Gat ekki sótt gögn');
+        } finally {
+          setLoading(false);
+        }
+
+        setData(json); 
+      }
+      fetchData();
+  }, []);
+
+  if (error) {
+    return (<div>Villa kom upp! {error}</div>);
+  }
+
+  if (loading) {
+    return (<div>Hleð inn gögn...</div>);
+  }
+  
 }
