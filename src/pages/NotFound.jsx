5c9c83d40843a07100153621d035406e4e945e78
siljaros1994 <70152073+siljaros1994@users.noreply.github.com>
2021-03-27 23:43:58 +0000

    Update NotFound.jsx

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,4 +1,14 @@
+import React from "react";
+import {Link} from 'react-router-dom'
+import s from './NotFound.scss';
 
 export function NotFound() {
   // TODO útfæra 404 síðu
+  return (
+    <div className={s.notfound}>
+      <h1 className={s.notfound_title}>404: error</h1>
+      <h2 className={s.notfound_text}>Síða fannst ekki</h2>
+      <Link to="/">Til baka á forsíðu</Link>
+    </div>
+  );
 }
