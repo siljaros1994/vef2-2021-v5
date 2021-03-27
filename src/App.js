cc49da02327b0a6ca6437a89aa8463790614a7fa
siljaros1994 <70152073+siljaros1994@users.noreply.github.com>
2021-03-27 23:43:33 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
 // TODO sækja og setja upp react router
-
-import { Layout } from './components/layout/Layout';
-
-import { Index } from './pages/Index';
-import { NewsPage } from './pages/News';
-import { NotFound } from './pages/NotFound';
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import Layout from './components/layout/Layout';
+import Index from './pages/Index';
+import NewsPage from './pages/News';
+import NotFound from './pages/NotFound';
 
 export default function App() {
   return (
     <Layout>
-      ...
+       <div className="App">
+        <section>
+          <Switch>
+            <Route exact path="/" component={Index} />
+            <Route exact path="/:id" component={NewsPage} />
+            <Route component={NotFound}/>
+          </Switch>
+        </section>
+      </div>
     </Layout>
   );
 }
