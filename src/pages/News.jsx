6ff2bb9b1f20a617f0b564deaffad60071db963d
siljaros1994 <70152073+siljaros1994@users.noreply.github.com>
2021-03-27 23:43:55 +0000

    Update News.jsx

--- src/pages/News.jsx
+++ src/pages/News.jsx
@@ -1,4 +1,12 @@
+import React from 'react';
+import { useParams } from 'react-router-dom';
+import { News } from '../components/news/News';
 
 export function NewsPage() {
   // TODO útfæra fréttasíðu
+  let id = useParams();
+  this.fetchData(id);
+  return (
+    <News category={id}/>
+  );
 }
