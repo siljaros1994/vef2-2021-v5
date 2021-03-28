ac8a5dcb8f404d602daafd513d67801cdd76adc5
siljaros1994 <70152073+siljaros1994@users.noreply.github.com>
2021-03-28 00:51:50 +0000

    Update Layout.jsx

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import s from './Layout.scss';
+
 // TODO sækja Sass
 
 export function Layout({children}) {
   // TODO setja upp layout fyrir vef
   return (
-    <div className={layout}>
+    <div className={s.layout}>
       <h1>RÚV fréttir</h1>
       <main className={s.layout_main}>{children}</main>
       <footer>
